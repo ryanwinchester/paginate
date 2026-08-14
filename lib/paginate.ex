5fc8c714efd1200ca3b7c0df4117b4f3ddd0fae1
Ryan Winchester <ryan@winchester.dev>
2026-08-14 20:05:50 -0300

    initial commit

---
+++ lib/paginate.ex
@@ -0,0 +1,18 @@
+defmodule Paginate do
+  @moduledoc """
+  Documentation for `Paginate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Paginate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/paginate_test.exs
@@ -0,0 +1,8 @@
+defmodule PaginateTest do
+  use ExUnit.Case
+  doctest Paginate
+
+  test "greets the world" do
+    assert Paginate.hello() == :world
+  end
+end
